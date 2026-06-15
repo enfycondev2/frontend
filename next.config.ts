891926201c87abcdb7bd03679082380b0f88597c
imssahadeb <saha.ptc.om@gmail.com>
2026-06-16 00:56:36 +0530

    feat: configure redirects from root and dashboard paths to dashboard district page

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard/district",
+        permanent: false,
+      },
+      {
+        source: "/dashboard",
+        destination: "/dashboard/district",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
